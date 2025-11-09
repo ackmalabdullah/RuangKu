6ad37054b4dbe26f9d8a96ac0bf8c0b96881fb7f
ackmalabdullah <122775505+ackmalabdullah@users.noreply.github.com>
2025-11-09 17:49:24 +0700

    memperbaiki footer dan dashboard

--- views/admin/dashboard/dashboard.php
+++ views/admin/dashboard/dashboard.php
@@ -1,16 +1,14 @@
  <?php
-  $required_role = 'admin';
+ $required_role = 'admin';
 
   require '../../../partials/mahasiswa/header.php';
   require '../../../partials/mahasiswa/sidebar.php';
   require '../../../partials/mahasiswa/navbar.php';
-
   ?>
-
  <div class="container-xxl flex-grow-1 container-p-y">
    <div class="row">
      <div class="col-lg-8 mb-4 order-0">
-       <div class="card"> 
+       <div class="card">
          <div class="d-flex align-items-end row">
            <div class="col-sm-7">
              <div class="card-body">
@@ -26,7 +24,7 @@
            <div class="col-sm-5 text-center text-sm-left">
              <div class="card-body pb-0 px-0 px-md-4">
                <img
-                 src="../../assets/assets_dashboard/assets/img/illustrations/man-with-laptop-light.png"
+                 src="../../../assets/assets_dashboard/assets/img/illustrations/man-with-laptop-light.png"
                  height="140"
                  alt="View Badge User"
                  data-app-dark-img="illustrations/man-with-laptop-dark.png"
@@ -44,7 +42,7 @@
                <div class="card-title d-flex align-items-start justify-content-between">
                  <div class="avatar flex-shrink-0">
                    <img
-                     src="../../assets/assets_dashboard/assets/img/icons/unicons/chart-success.png"
+                     src="../../../assets/assets_dashboard/assets/img/icons/unicons/chart-success.png"
                      alt="chart success"
                      class="rounded" />
                  </div>
@@ -76,7 +74,7 @@
                <div class="card-title d-flex align-items-start justify-content-between">
                  <div class="avatar flex-shrink-0">
                    <img
-                     src="../../assets/assets_dashboard/assets/img/icons/unicons/wallet-info.png"
+                     src="../../../assets/assets_dashboard/assets/img/icons/unicons/wallet-info.png"
                      alt="Credit Card"
                      class="rounded" />
                  </div>
@@ -168,7 +166,7 @@
              <div class="card-body">
                <div class="card-title d-flex align-items-start justify-content-between">
                  <div class="avatar flex-shrink-0">
-                   <img src="../../assets/assets_dashboard/assets/img/icons/unicons/paypal.png" alt="Credit Card" class="rounded" />
+                   <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/paypal.png" alt="Credit Card" class="rounded" />
                  </div>
                  <div class="dropdown">
                    <button
@@ -197,7 +195,7 @@
              <div class="card-body">
                <div class="card-title d-flex align-items-start justify-content-between">
                  <div class="avatar flex-shrink-0">
-                   <img src="../../assets/assets_dashboard/assets/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
+                   <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
                  </div>
                  <div class="dropdown">
                    <button
@@ -372,7 +370,7 @@
              <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
                <div class="d-flex p-4 pt-3">
                  <div class="avatar flex-shrink-0 me-3">
-                   <img src="../../assets/assets_dashboard/assets/img/icons/unicons/wallet.png" alt="User" />
+                   <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/wallet.png" alt="User" />
                  </div>
                  <div>
                    <small class="text-muted d-block">Total Balance</small>
@@ -401,6 +399,126 @@
        </div>
      </div>
      <!--/ Expense Overview -->
+
+     <!-- Transactions -->
+     <div class="col-md-6 col-lg-4 order-2 mb-4">
+       <div class="card h-100">
+         <div class="card-header d-flex align-items-center justify-content-between">
+           <h5 class="card-title m-0 me-2">Transactions</h5>
+           <div class="dropdown">
+             <button
+               class="btn p-0"
+               type="button"
+               id="transactionID"
+               data-bs-toggle="dropdown"
+               aria-haspopup="true"
+               aria-expanded="false">
+               <i class="bx bx-dots-vertical-rounded"></i>
+             </button>
+             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="transactionID">
+               <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
+               <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
+               <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
+             </div>
+           </div>
+         </div>
+         <div class="card-body">
+           <ul class="p-0 m-0">
+             <li class="d-flex mb-4 pb-1">
+               <div class="avatar flex-shrink-0 me-3">
+                 <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/paypal.png" alt="User" class="rounded" />
+               </div>
+               <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                 <div class="me-2">
+                   <small class="text-muted d-block mb-1">Paypal</small>
+                   <h6 class="mb-0">Send money</h6>
+                 </div>
+                 <div class="user-progress d-flex align-items-center gap-1">
+                   <h6 class="mb-0">+82.6</h6>
+                   <span class="text-muted">USD</span>
+                 </div>
+               </div>
+             </li>
+             <li class="d-flex mb-4 pb-1">
+               <div class="avatar flex-shrink-0 me-3">
+                 <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
+               </div>
+               <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                 <div class="me-2">
+                   <small class="text-muted d-block mb-1">Wallet</small>
+                   <h6 class="mb-0">Mac'D</h6>
+                 </div>
+                 <div class="user-progress d-flex align-items-center gap-1">
+                   <h6 class="mb-0">+270.69</h6>
+                   <span class="text-muted">USD</span>
+                 </div>
+               </div>
+             </li>
+             <li class="d-flex mb-4 pb-1">
+               <div class="avatar flex-shrink-0 me-3">
+                 <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/chart.png" alt="User" class="rounded" />
+               </div>
+               <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                 <div class="me-2">
+                   <small class="text-muted d-block mb-1">Transfer</small>
+                   <h6 class="mb-0">Refund</h6>
+                 </div>
+                 <div class="user-progress d-flex align-items-center gap-1">
+                   <h6 class="mb-0">+637.91</h6>
+                   <span class="text-muted">USD</span>
+                 </div>
+               </div>
+             </li>
+             <li class="d-flex mb-4 pb-1">
+               <div class="avatar flex-shrink-0 me-3">
+                 <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/cc-success.png" alt="User" class="rounded" />
+               </div>
+               <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                 <div class="me-2">
+                   <small class="text-muted d-block mb-1">Credit Card</small>
+                   <h6 class="mb-0">Ordered Food</h6>
+                 </div>
+                 <div class="user-progress d-flex align-items-center gap-1">
+                   <h6 class="mb-0">-838.71</h6>
+                   <span class="text-muted">USD</span>
+                 </div>
+               </div>
+             </li>
+             <li class="d-flex mb-4 pb-1">
+               <div class="avatar flex-shrink-0 me-3">
+                 <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/wallet.png" alt="User" class="rounded" />
+               </div>
+               <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                 <div class="me-2">
+                   <small class="text-muted d-block mb-1">Wallet</small>
+                   <h6 class="mb-0">Starbucks</h6>
+                 </div>
+                 <div class="user-progress d-flex align-items-center gap-1">
+                   <h6 class="mb-0">+203.33</h6>
+                   <span class="text-muted">USD</span>
+                 </div>
+               </div>
+             </li>
+             <li class="d-flex">
+               <div class="avatar flex-shrink-0 me-3">
+                 <img src="../../../assets/assets_dashboard/assets/img/icons/unicons/cc-warning.png" alt="User" class="rounded" />
+               </div>
+               <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                 <div class="me-2">
+                   <small class="text-muted d-block mb-1">Mastercard</small>
+                   <h6 class="mb-0">Ordered Food</h6>
+                 </div>
+                 <div class="user-progress d-flex align-items-center gap-1">
+                   <h6 class="mb-0">-92.45</h6>
+                   <span class="text-muted">USD</span>
+                 </div>
+               </div>
+             </li>
+           </ul>
+         </div>
+       </div>
+     </div>
+     <!--/ Transactions -->
    </div>
  </div>
  <?php
